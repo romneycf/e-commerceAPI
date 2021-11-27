a3cd9e64a48457ead87faab9d91f8da4c9cee949
Romney Freire <romneycf@gmail.com>
2021-11-27 12:41:05 -0300

    First commit

---
+++ routes/pedidos.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+//RETORNA TODOS OS PEDIDOS
+router.get("/", (req, res, next) => {
+  res.status(200).send({
+    mensagem: "Retorna os pedidos",
+  });
+});
+
+//INSERE UM PEDIDO
+router.post("/", (req, res, next) => {
+    const pedido= {
+        id_produto: req.body.id_produto,
+        quantidade: req.body.quantidade
+    }
+  res.status(201).send({
+    mensagem: "O pedido foi criado",
+    pedidoCriado: pedido
+  });
+});
+
+//RETORNA OS DADOS DE UM PEDIDO
+router.get("/:pedido", (req, res, next) => {
+  const id = req.params.id_pedido;
+  res.status(200).send({
+    mensagem: "Detalhes do Pedido",
+    id_pedido: id,
+  });
+});
+
+//EXCLUI UM PEDIDO
+router.delete("/", (req, res, next) => {
+  res.status(201).send({
+    mensagem: "Pedido excluído",
+  });
+});
+
+module.exports = router;
